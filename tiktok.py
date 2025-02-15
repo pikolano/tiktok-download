f2d62882f8fbc639288c7c7436e5a51bef077875
pikolano <141550872+pikolano@users.noreply.github.com>
2025-02-15 21:22:17 +0200

    Bot

---
+++ tiktok.py
@@ -0,0 +1,63 @@
+import os
+import yt_dlp
+import asyncio
+import time
+from telegram import Update, InlineQueryResultVideo
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, InlineQueryHandler
+
+TOKEN = 'TOKEN'
+
+async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query.strip()
+
+    if not query or not ('tiktok.com' in query or 'vm.tiktok.com' in query):
+        return
+
+    try:
+        video_path = await asyncio.to_thread(download_video, query)
+        
+        if video_path:
+            with open(video_path, 'rb') as video_file:
+                sent_video = await context.bot.send_video(
+                    chat_id=update.inline_query.from_user.id, 
+                    video=video_file
+                )
+            
+            result = InlineQueryResultVideo(
+                id=str(time.time()),
+                video_url=sent_video.video.file_id,  
+                mime_type="video/mp4",
+                thumbnail_url="https://modkit.ct.ws/botimage.jpg?i=2", 
+                title="Держи свое видео!"
+            )
+            await update.inline_query.answer([result], cache_time=0)
+            os.remove(video_path)
+
+    except Exception as e:
+        print(f"Ошибка при обработке инлайн-запроса: {e}")
+
+def download_video(url: str) -> str:
+    ydl_opts = {
+        'format': 'best[ext=mp4]',  
+        'outtmpl': 'video.mp4',     
+        'noplaylist': True,         
+        'quiet': True,              
+        'restrict_filenames': True,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            return 'video.mp4'
+    except Exception as e:
+        print(f'Ошибка загрузки: {e}')
+        return None
+
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(InlineQueryHandler(inline_query))  
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
